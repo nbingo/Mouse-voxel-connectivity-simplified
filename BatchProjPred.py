84d09fda716d42767133e967925f3b49243554c8
nbingo <5738735+nbingo@users.noreply.github.com>
2020-04-02 16:36:30 -0700

    Updated requirements and now reading from desired CSV file for secondary target brain areas

--- BatchProjPred.py
+++ BatchProjPred.py
@@ -1,14 +1,12 @@
 from ProjPredictor import ProjPredictor
 import os
+import pandas as pd
 from tqdm import tqdm
 
 image_path = '/transformix_output_ilastik/result_fixed.tif'
-areas = ['Somatomotor areas',
-         'Visual areas',
-         'Orbital area',
-         'Prelimbic area',
-         'Infralimbic area'
-         ]
+areas = pd.read_csv('annotation_info_0118_1327.csv')
+areas = areas[areas['consider'] == 1]['name'].values.tolist()
+
 nuclei = [('DN', 'Dentate nucleus'), ('FN', 'Fastigial nucleus'), ('IN', 'Interposed nucleus')]
 pp = ProjPredictor(verbose=False)
 # area_filter = 'Ventral medial nucleus of the thalamus'
@@ -26,16 +24,20 @@ for nucleus in nuclei:
         pp.save_proj_by_area(structure_name=areas,
                              normalize_source=True,
                              normalize_target=True,
-                             fname=f'proj_by_area/{nucleus[0]}{brain[-3:]}_filter-{area_filter}_both-norm_proj_by_area.pickle')
+                             fname=f'proj_by_area_justus/{nucleus[0]}{brain[-3:]}_filter-{area_filter}'
+                                   f'_both-norm_proj_by_area.pickle')
         pp.save_proj_by_area(structure_name=areas,
                              normalize_source=False,
                              normalize_target=True,
-                             fname=f'proj_by_area/{nucleus[0]}{brain[-3:]}_filter-{area_filter}_target-norm_proj_by_area.pickle')
+                             fname=f'proj_by_area_justus/{nucleus[0]}{brain[-3:]}_filter-{area_filter}'
+                                   f'_target-norm_proj_by_area.pickle')
         pp.save_proj_by_area(structure_name=areas,
                              normalize_source=True,
                              normalize_target=False,
-                             fname=f'proj_by_area/{nucleus[0]}{brain[-3:]}_filter-{area_filter}_source-norm_proj_by_area.pickle')
+                             fname=f'proj_by_area_justus/{nucleus[0]}{brain[-3:]}_filter-{area_filter}'
+                                   f'_source-norm_proj_by_area.pickle')
         pp.save_proj_by_area(structure_name=areas,
                              normalize_source=False,
                              normalize_target=False,
-                             fname=f'proj_by_area/{nucleus[0]}{brain[-3:]}_filter-{area_filter}_no-norm_proj_by_area.pickle')
+                             fname=f'proj_by_area_justus/{nucleus[0]}{brain[-3:]}_filter-{area_filter}'
+                                   f'_no-norm_proj_by_area.pickle')
